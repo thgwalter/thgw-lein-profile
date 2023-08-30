b881245d5b810942434296f83195fec9f8b27615
thgwalter <thg.walter@gmail.com>
2023-08-30 18:22:29 +0200

    Upgrades.

--- profiles.clj
+++ profiles.clj
@@ -8,10 +8,10 @@
  
  :repl {:dependencies [[nrepl/nrepl "1.0.0"]]
 
-        :plugins [[cider/cider-nrepl "0.36.1"]
+        :plugins [[cider/cider-nrepl "0.37.0"]
                   [cider/orchard "0.14.2"]
                   [refactor-nrepl/refactor-nrepl "3.9.0"]
-                  [mx.cider/enrich-classpath "1.14.0"]]
+                  [mx.cider/enrich-classpath "1.15.3"]]
         :middleware [cider.enrich-classpath/middleware]
         :enrich-classpath {:classifiers #{"sources" "javadoc"}}
 
