40ddd7dbf64296ba7b0dc67a2e7f11570d7b7e87
thgwalter <thg.walter@gmail.com>
2023-09-21 08:45:29 +0200

    Upgrades.

--- profiles.clj
+++ profiles.clj
@@ -2,19 +2,19 @@
                        [org.slf4j/slf4j-simple "RELEASE"]]
         :aliases {"outdated" ["run" "-m" "antq.core"]}
 
-        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/20.32.11/lib/src.zip"]
+        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/21.28.85/lib/src.zip"]
 
         :local-repo "c:/repo"}
  
  :repl {:dependencies [[nrepl/nrepl "1.0.0"]]
 
-        :plugins [[cider/cider-nrepl "0.37.0"]
-                  [cider/orchard "0.14.2"]
+        :plugins [[cider/cider-nrepl "0.37.1"]
+                  [cider/orchard "0.15.0"]
                   [refactor-nrepl/refactor-nrepl "3.9.0"]
-                  [mx.cider/enrich-classpath "1.16.0"]]
+                  [mx.cider/enrich-classpath "1.17.1"]]
         :middleware [cider.enrich-classpath/middleware]
         :enrich-classpath {:classifiers #{"sources" "javadoc"}}
 
-        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/20.32.11/lib/src.zip"]
+        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/21.28.85/lib/src.zip"]
         
         :local-repo "c:/repo"}}
