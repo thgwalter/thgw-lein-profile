b98b86bb3bf19b9fefae650a1db4bcaa8daa7748
thgwalter <thg.walter@gmail.com>
2023-10-24 12:47:32 +0200

    Upgrades.

--- profiles.clj
+++ profiles.clj
@@ -9,7 +9,7 @@
  :repl {:dependencies [[nrepl/nrepl "1.0.0"]]
 
         :plugins [[cider/cider-nrepl "0.40.0"]
-                  [cider/orchard "0.16.1"]
+                  [cider/orchard "0.17.0"]
                   [refactor-nrepl/refactor-nrepl "3.9.0"]
                   [mx.cider/enrich-classpath "1.18.2"]]
         :middleware [cider.enrich-classpath/middleware]
