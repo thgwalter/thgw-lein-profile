0c3e84a8ee4a3cf3d852d8276026c4a91abef6dc
thgwalter <thg.walter@gmail.com>
2024-03-09 11:30:42 +0100

    Upgrades.

--- profiles.clj
+++ profiles.clj
@@ -1,20 +1,25 @@
-{:user {:dependencies [[com.github.liquidz/antq "RELEASE"]
-                       [org.slf4j/slf4j-simple "RELEASE"]]
+{:user {:dependencies [[com.github.liquidz/antq "2.8.1185"]
+                       [org.slf4j/slf4j-api "2.0.12"]
+                       [org.slf4j/slf4j-simple "2.0.12"]]
         :aliases {"outdated" ["run" "-m" "antq.core"]}
 
-        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/21.32.17/lib/src.zip"]
+        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/21.32.17/lib/src.zip"
+                         "C:/Users/sib_twalter/scoop/apps/zulufx-jdk/21.32.17/lib/src.zip"
+                         "/home/util/opt/zulu21.30.15-ca-jdk21.0.1-linux_x64/lib/src.zip"]
 
         :local-repo "c:/repo"}
  
  :repl {:dependencies [[nrepl/nrepl "1.1.1"]]
 
-        :plugins [[cider/cider-nrepl "0.45.0"]
-                  [cider/orchard "0.21.0"]
-                  [refactor-nrepl/refactor-nrepl "3.9.1"]
+        :plugins [[cider/cider-nrepl "0.46.0"]
+                  [cider/orchard "0.23.0"]
+                  [refactor-nrepl/refactor-nrepl "3.10.0"]
                   [mx.cider/enrich-classpath "1.19.0"]]
         :middleware [cider.enrich-classpath/middleware]
         :enrich-classpath {:classifiers #{"sources" "javadoc"}}
 
-        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/21.32.17/lib/src.zip"]
+        :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/21.32.17/lib/src.zip"
+                         "C:/Users/sib_twalter/scoop/apps/zulufx-jdk/21.32.17/lib/src.zip"
+                         "/home/util/opt/zulu21.30.15-ca-jdk21.0.1-linux_x64/lib/src.zip"]
         
         :local-repo "c:/repo"}}
