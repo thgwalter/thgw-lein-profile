9ea55f05173bb326af8559a67234881c1971548e
thgwalter <thg.walter@gmail.com>
2025-01-30 15:44:07 +0100

    Upgrades.

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[com.github.liquidz/antq "2.11.1260"]
+{:user {:dependencies [[com.github.liquidz/antq "2.11.1264"]
                        [org.slf4j/slf4j-api "2.0.16"]
                        [org.slf4j/slf4j-simple "2.0.16"]]
         :aliases {"outdated" ["run" "-m" "antq.core"]}
@@ -10,10 +10,10 @@
         ;;:local-repo "c:/repo"
         }
  
- :repl {:dependencies [[nrepl/nrepl "1.3.0"]]
+ :repl {:dependencies [[nrepl/nrepl "1.3.1"]]
 
-        :plugins [[cider/cider-nrepl "0.50.3"]
-                  [cider/orchard "0.29.0" #_"0.27.2"]
+        :plugins [[cider/cider-nrepl "0.52.0"]
+                  [cider/orchard "0.30.0" #_"0.27.2"]
                   [refactor-nrepl/refactor-nrepl "3.10.0"]
                   [mx.cider/enrich-classpath "1.19.3"]]
         :middleware [cider.enrich-classpath/middleware]
