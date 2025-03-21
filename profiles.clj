b6e79ff66fb537c48419d5e0709be3ade2b5add9
thgwalter <thg.walter@gmail.com>
2025-03-21 15:51:08 +0100

    Upgrades.

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
-{:user {:dependencies [[com.github.liquidz/antq "2.11.1264"]
-                       [org.slf4j/slf4j-api "2.0.16"]
-                       [org.slf4j/slf4j-simple "2.0.16"]]
+{:user {:dependencies [[com.github.liquidz/antq "2.11.1276"]
+                       [org.slf4j/slf4j-api "2.0.17"]
+                       [org.slf4j/slf4j-simple "2.0.17"]]
         :aliases {"outdated" ["run" "-m" "antq.core"]}
 
         :resource-paths ["C:/Users/Utilisateur/scoop/apps/zulufx-jdk/current/lib/src.zip"
@@ -12,8 +12,8 @@
  
  :repl {:dependencies [[nrepl/nrepl "1.3.1"]]
 
-        :plugins [[cider/cider-nrepl "0.52.0"]
-                  [cider/orchard "0.30.0" #_"0.27.2"]
+        :plugins [[cider/cider-nrepl "0.53.0"]
+                  [cider/orchard "0.31.1"]
                   [refactor-nrepl/refactor-nrepl "3.10.0"]
                   [mx.cider/enrich-classpath "1.19.3"]]
         :middleware [cider.enrich-classpath/middleware]
